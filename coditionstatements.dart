b1091cdecdf34a703128bc08f570d290d2e3da11
vidhya <vidhyaavsr2416@gmail.com>
2023-03-19 02:47:33 +0530

    third commit

---
+++ coditionstatements.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  var a = 4;
+  var b = 5;
+  var small = a<b? a:b;
+  print("$small is smaller");
+var c = 15;
+var h = 12;
+ var large = c>h??c>h;
+ print("$large is large");
+
+
+}
---
+++ forloop.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  for( var i=0;i<10;i++)
+  {
+  print(i);
+  }
+List myname = ["vidhya", "sagar"];
+for(var name in myname){
+  print(name);
+}
+
+}
