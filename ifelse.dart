2bf004d96cb7d1e3795cfa58ba2e9199e83a441f
vidhya <vidhyaavsr2416@gmail.com>
2023-03-18 16:27:56 +0530

    second commit

---
+++ ifelse.dart
@@ -0,0 +1,25 @@
+void main()
+{
+    var sal = 150;
+    if(sal>200)
+    {
+        print("you got promotion");
+    }
+    else
+    {
+        print("you need to work hard");
+    }
+    var mar = 70;
+    if(mar>100)
+    {
+        print("a+");
+    }
+    else if(mar>=80&&mar<=90)
+    {
+       print("a");
+    }
+    else
+    {
+        print("invalid marks");
+    }
+    }
